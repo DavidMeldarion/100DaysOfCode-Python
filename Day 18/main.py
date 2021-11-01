56b004d91e9520816022fb56ecabbb41862aed81
David Cunningham <eiftsasfc@gmail.com>
2021-11-01 10:36:59 -0500

    Final

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,73 +1,31 @@
-"""Turtle"""
-from turtle import Turtle, Screen
+"""Hirst Painting with Turtle"""
 import random
+import turtle as t
+from colors import color_list
 
-tim = Turtle()
+def random_color():
+    "Choose a random color from color_list"
+    new_color=random.choice(color_list)
+    return new_color
 
-tim.color("blue")
-tim.shape("turtle")
-tim.pensize(1)
-tim.speed(0)
+tim=t.Turtle()
 
-screen = Screen()
+screen=t.Screen()
 screen.colormode(255)
 
-def generate_random_color():
-    """Make Tim a random color"""
-    tim.color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+def draw_horizontal():
+    """Make Tim draw a horizontal line of dots with a random color and width of 10 dots."""
+    for _ in range(1,11):
+        color=random_color()
+        tim.dot(20,color)
+        tim.pd()
+        tim.pu()
+        tim.forward(50)
 
-def move(chosen_direction):
-    """Make Tim walk north"""
-    generate_random_color()
-    tim.seth(chosen_direction)
-    tim.forward(100)
-
-directions=[
-    90,
-    270,
-    0,
-    180
-]
-
-#Creating Spirograph
-for i in range(1,360,10):
-    generate_random_color()
-    tim.circle(100)
-    tim.seth(i)
-
-#Creating Random Walk
-# for i in range(200):
-#     chosen_direction=random.choice(directions)
-#     move(chosen_direction)
-
-
-# def shape(dimensions_dict):
-#     """Draws shape based on input dict"""
-#     generate_random_color()
-#     tim.setheading(0)
-#     sides=dimensions_dict["sides"]
-#     increment=0
-#     while increment < sides:
-#         tim.forward(100)
-#         tim.right(dimensions_dict["angle"])
-#         tim.forward(100)
-#         increment+=1
-
-# dimensions=[
-#     {"sides": 3,"angle": 120},
-#     {"sides":4,"angle": 90},
-#     {"sides":5,"angle": 72},
-#     {"sides":6,"angle": 60},
-#     {"sides":7,"angle": 51.4285714},
-#     {"sides":8,"angle": 45},
-#     {"sides":9,"angle": 40}
-#     ]
-
-# i=0
-# LENGTH=len(dimensions)
-# while i < LENGTH:
-#     shape(dimensions[i])
-#     i+=1
-
+y_pos=0
+for i in range(1,11):
+    tim.setposition(0,y_pos)
+    draw_horizontal()
+    y_pos+=50
 
 screen.exitonclick()
