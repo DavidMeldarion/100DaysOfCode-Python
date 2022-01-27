2b37bb5c988ab5e0cf443aa3b6afb306ff4de8f3
David Cunningham <eiftsasfc@gmail.com>
2022-01-27 08:52:48 -0600

    initial commit

---
+++ Day 24/scoreboard.py
@@ -0,0 +1,43 @@
+"""Creates scoreboard"""
+from turtle import Turtle
+
+ALIGNMENT = "center"
+
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    """Initializes the scoreboard"""
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.setposition(0,260)
+        self.score = 0
+        with open("data.txt") as file:
+            self.high_score = file.read()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        """Updates Scoreboard"""
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT,  font=FONT)
+
+    def increase_score(self):
+        """Increases the score"""
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def game_over(self):
+        """Alerts the user that the game is over"""
+        self.penup()
+        self.hideturtle()
+        self.setposition(0,0)
+        self.color("white")
+        self.write("GAME OVER", align=ALIGNMENT,  font=FONT)
