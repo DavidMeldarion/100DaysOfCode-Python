4bcaea6bdc02f1f6ae8df5102d03d323b8c549b4
David Cunningham <eiftsasfc@gmail.com>
2022-01-31 14:24:46 -0600

    final push

--- Day 26/main.py
+++ Day 26/main.py
@@ -8,7 +8,7 @@ nato_data_frame = pandas.DataFrame(nato_csv)
 
 nato_alphabet_dict = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
 
-print (nato_alphabet_dict)
+# print (nato_alphabet_dict)
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
